a6dc646b3d63db18120936eb3cf9c8421e37f78a
Kevin Garcia <kevin2001garciacarrasco@gmail.com>
2024-04-04 09:23:16 -0600

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+    const [inputValue, setinputValue] = useState('');
+    const onInputChange = ({ target }) => {
+        console.log(target.value);
+        setinputValue(target.value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+        onNewCategory(inputValue.trim())
+        setinputValue('')
+
+    }
+    return (
+        <form onSubmit={(event) => { onSubmit(event) }}>
+            <input
+                type='text'
+                placeholder='Buscar gifs'
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+
+    )
+}
+
